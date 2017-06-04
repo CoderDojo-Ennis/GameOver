d5a3d0e3f4003c057aa1a4e657614adaf64d512e
Russ Painter <russp@geekymonkey.com>
2017-06-04 15:48:48 +0100

    Video playlists - round robin from userprefs

--- GameOver/Assets/GameManager.cs
+++ GameOver/Assets/GameManager.cs
@@ -4,23 +4,49 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public GeekyMonkeyVideoPlaylist VideoPlaylist_War;
+
     GeekyMonkeyVideoDirector videoDirector;
+
+    GameObject Menu;
+    GameObject MainCamera;
+    GameObject VideoCamera;
 
 	// Use this for initialization
 	void Start () {
         videoDirector = GeekyMonkeyVideoDirector.Instance;
 
-        this.Delay(3, () =>
-        {
-            videoDirector.PlayClip().Then(() =>
+        // Disable everything but the intro video
+        Menu = GameObject.Find("Menu");
+        Menu.SetActive(false);
+
+        MainCamera = GameObject.Find("Main Camera");
+        MainCamera.SetActive(false);
+
+        // Start intro video
+        VideoCamera = GameObject.Find("VideoCamera");
+        VideoCamera.GetComponent<Camera>().enabled = true;
+        //this.Delay(3, () =>
+        //{
+            VideoPlaylist_War.PlayNext().Then(() =>
             {
+                Menu.SetActive(true);
                 Debug.Log("video done!");
             });
-        });
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+            // Allow for video fade-in, then activate menu in the background
+            this.Delay(3, () =>
+            {
+                Menu.SetActive(true);
+                MainCamera.SetActive(true);
+            });
+        //});
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
