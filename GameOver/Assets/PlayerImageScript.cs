c0f670c845896e12d5e41a5e4f9de27212ef724b
Russ Painter <russp@geekymonkey.com>
2017-06-25 22:57:36 +0100

    Transition from WarInstructions to War. Display kinect player in war

---
+++ GameOver/Assets/PlayerImageScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerImageScript : MonoBehaviour
+{
+    private MeshRenderer MeshRenderer;
+
+    public void Awake()
+    {
+        this.MeshRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+    void Update()
+    {
+        // Render the kinect color image onto a quad
+        BackgroundRemovalManager backManager = BackgroundRemovalManager.Instance;
+
+        if (backManager && backManager.IsBackgroundRemovalInitialized())
+        {
+            var tex = backManager.GetForegroundTex();
+            MeshRenderer.material.SetTexture("_MainTex", tex);
+        }
+    }
+}
