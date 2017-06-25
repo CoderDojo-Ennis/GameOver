f3de4fb8d357ce41ab1b29708dbb456ada467e8e
Russ Painter <russp@geekymonkey.com>
2017-06-25 14:52:23 +0100

    Avatar animation in invitation

---
+++ GameOver/Assets/AvatarScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class AvatarScript : MonoBehaviour
+{
+    private SpriteRenderer SpriteRenderer;
+
+    public void Awake()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public GmDelayPromise GlideX(float startX, float targetX, float seconds)
+    {
+        int steps = 100;
+        float step = 0;
+
+        transform.localPosition = new Vector3(startX, transform.localPosition.y, transform.localPosition.z);
+
+        return this.Repeat(seconds / steps, steps, () =>
+        {
+            step++;
+            var x = Mathf.Lerp(startX, targetX, step / steps);
+            Debug.Log("X=" + x + " @ step " + step);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+        }, true);
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        SpriteRenderer.sprite = sprite;
+    }
+}
--- GameOver/Assets/Scenes/InviteMenu.cs
+++ GameOver/Assets/Scenes/InviteMenu.cs
@@ -10,8 +10,23 @@ public class InviteMenu : BaseMenu
     [Header("Sounds")]
     public AudioClip PlayerLostSound;
 
+    [Header("Avatar")]
+    public Sprite GlideSprite;
+    public Sprite IdleSprite;
+
+    private AvatarScript Avatar;
+
     /// <summary>
     /// Awake (before start)
+    /// </summary>
+    public void Awake()
+    {
+        Avatar = GetComponentInChildren<AvatarScript>(true);
+        Avatar.gameObject.layer = this.gameObject.layer;
+    }
+
+    /// <summary>
+    /// Start (after awake)
     /// </summary>
     public void Start()
     {
@@ -30,6 +45,12 @@ public class InviteMenu : BaseMenu
         InstructionText.Type(this, TypingSeconds, true, () =>
         {
             AudioManager.Instance.PlayTypeCharacter();
+        });
+
+        Avatar.SetSprite(GlideSprite);
+        Avatar.GlideX(-7, 0, 1).Then(() =>
+        {
+            Avatar.SetSprite(IdleSprite);
         });
     }
 }
