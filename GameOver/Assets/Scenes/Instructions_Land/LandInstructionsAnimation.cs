08553c447750b1808e03abc1932c4bf304d862a7
Russ Painter <russp@geekymonkey.com>
2017-07-04 21:10:07 +0100

    Land videos and coming soon animation

---
+++ GameOver/Assets/Scenes/Instructions_Land/LandInstructionsAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LandInstructionsAnimation : MonoBehaviour
+{
+    public float SceneStartDelay = 1f;
+    public SpriteRenderer LightColumn;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Full health
+        PlayerScript.Instance.SetInitialHealth();
+
+        // Start animation
+        this.Delay(SceneStartDelay, () =>
+        {
+            LightColumn.FadeAlpha(this, LightColumn.color.a, 0, 2f, true);
+        });
+    }
+}
