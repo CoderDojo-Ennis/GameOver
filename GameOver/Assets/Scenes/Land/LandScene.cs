f41c59d4bfd080d3bffa3da51f086020b7f38d9e
Quinn Painter <quinn@geekymonkey.com>
2018-06-10 13:37:20 +0100

    Basic land scene with lighting, grass etc

---
+++ GameOver/Assets/Scenes/Land/LandScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandScene : BaseGameScene
+{
+	new void Start ()
+    {
+        base.Start();
+	}
+	
+	public override void FirstUpdate ()
+    {
+        base.FirstUpdate();
+	}
+
+    new void Update()
+    {
+        base.Update();
+    }
+}
